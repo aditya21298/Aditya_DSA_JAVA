c0a610527949aea941d42eeccae8fcd21faa4b28
aditya <aditya21298@gmail.com>
2022-10-22 22:24:40 +0530

    Initial commit with LinkedList untill append method

---
+++ src/datastructures/Main.java
@@ -0,0 +1,12 @@
+package datastructures;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList myLinkedList = new LinkedList(4);
+        myLinkedList.append(5);
+        myLinkedList.getHead();
+        myLinkedList.getTail();
+        myLinkedList.getLength();
+        myLinkedList.printList();
+    }
+}
